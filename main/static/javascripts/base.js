27208d7cf95f0d11705f80ea1ef8d059e0dacdce
John Spencer <johnspencer15@gmail.com>
2016-11-17 00:46:38 -0500

    Add some margin to top of login and signup

--- main/static/javascripts/base.js
+++ main/static/javascripts/base.js
@@ -2,7 +2,4 @@ $(function() {
 
     // FIX ME
     // Hack to make navbar work on mobile. Bootstrap should do this for us.
-    $('.navbar-toggle').click(function() {
-        $('.collapse').toggle();
-    });
 });
