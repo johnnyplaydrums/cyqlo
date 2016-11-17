8771e4ce97d613842fe89ad0c01003d8c65e143d
John Spencer <johnspencer15@gmail.com>
2016-11-16 20:16:32 -0500

    Hacky fix for bootstrap navbar issue

---
+++ main/static/javascripts/base.js
@@ -0,0 +1,8 @@
+$(function() {
+
+    // FIX ME
+    // Hack to make navbar work on mobile. Bootstrap should do this for us.
+    $('.navbar-toggle').click(function() {
+        $('.collapse').toggle();
+    });
+});
