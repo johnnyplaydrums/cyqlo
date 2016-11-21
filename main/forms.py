27bc9d8babb13323432239d11464001d00a00695
wujunhua <junhua.wu@outlook.com>
2016-11-21 16:46:36 -0500

    working signup 2.0

--- main/forms.py
+++ main/forms.py
@@ -1,20 +1,8 @@
-import re
-from django import forms
-from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.forms import ModelForm
+from main.models import User
 
-class RegistrationForm(forms.Form):
-    first_name = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=50)), label=_("First Name"))
-    last_name = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=50)), label=_("Last Name"))
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=50)), label=_("Email address"))
-    password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=256, render_value=False)), label=_("Password"))
+class RegistrationForm(ModelForm):
 
-    def clean_email(self):
-        '''
-        Validate that the email is not already exist
-        '''
-        try:
-            email = User.objects.get(email=self.cleaned_data['email'])
-        except User.DoesNotExist:
-            return self.cleaned_data['email']
-        raise forms.ValidationError(_("The email you try to input already exists. Please try another one."))
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password']
--- main/views.py
+++ main/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 from main.forms import *
 from django.views.decorators.csrf import csrf_protect
 from django.http import HttpResponseRedirect
-#from django.template import RequestContext
-
 
 # Create your views here.
 
@@ -19,17 +17,13 @@ def login(request):
 
 @csrf_protect
 def signup(request):
-    """ User signup page """
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            """ username is linked to email, also needed a username argument for create_user() """
-            user = User.objects.create_user(username=form.cleaned_data['email'], first_name=form.cleaned_data['first_name'], last_name=form.cleaned_data['last_name'], email=form.cleaned_data['email'], password=form.cleaned_data['password'])
+            user = User.objects.create(first_name=form.cleaned_data['first_name'], last_name=form.cleaned_data['last_name'], email=form.cleaned_data['email'], password=form.cleaned_data['password'])
             return HttpResponseRedirect('/')
     else:
         form = RegistrationForm()
-    #variables = RequestContext(request, {'form': form})
-    #return render('signup.html', variables)
     return render(request, 'signup.html')
 
 def about(request):
