6d8c0cece771d6eae86eb1901ccef71de8a97eb7
Sammauel <S.Quintan4@gmail.com>
2016-11-22 14:51:45 -0500

    Form Test WIP

--- main/tests/unittests/test_forms.py
+++ main/tests/unittests/test_forms.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+from main.models import User
+
+class FormsTestPage(TestCase):
+
+    def setUp(self):
+        self.mBanks=User.objects.create(first_name ="James", last_name="Glover", email="jglover@email.com",
+                password="jglover123")
+        self.tBarnett=User.objects.create(first_name="Alyssa", last_name="Ramos", email="aramos@email.com",
+                password="aramos123")
