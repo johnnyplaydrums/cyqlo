40652c4fd75fe57cb48e9687fbb562a7cca5872a
mhendri <mhendrickson91@me.com>
2016-11-25 16:40:44 -0500

    taking script tag  out of html and js file

--- main/static/javascripts/westside.js
+++ main/static/javascripts/westside.js
@@ -1,68 +1,65 @@
-<script>
-    function initMap() {
-      var directionsService = new google.maps.DirectionsService;
-      var directionsDisplay = new google.maps.DirectionsRenderer;
-      var map = new google.maps.Map(document.getElementById('map'));
-      directionsDisplay.setMap(map);
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'));
+  directionsDisplay.setMap(map);
 
-      var button = document.getElementById("buttonid");
-      var control = document.getElementById('floating-panel');
-      control.style.display = 'block';
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(control);
+  var button = document.getElementById("buttonid");
+  var control = document.getElementById('floating-panel');
+  control.style.display = 'block';
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(control);
 
-      button.addEventListener("click", function() {
-      directionsDisplay.setPanel(document.getElementById('right-panel'));
-      });
+  button.addEventListener("click", function() {
+  directionsDisplay.setPanel(document.getElementById('right-panel'));
+  });
 
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-      document.getElementById('submit').addEventListener('click', function() {
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+  document.getElementById('submit').addEventListener('click', function() {
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var waypts = [];
+  var checkboxArray = document.getElementById('waypoints');
+  for (var i = 0; i < checkboxArray.length; i++) {
+    if (checkboxArray.options[i].selected) {
+      waypts.push({
+        location: checkboxArray[i].value,
+        stopover: true
       });
     }
+  }
 
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+    origin: {lat:40.703104, lng:-74.016847},
+    destination: {lat: 40.850089, lng:-73.946785},
+    waypoints:waypts,
+    optimizeWaypoints:true,
+    travelMode:'BICYCLING'
 
-      var waypts = [];
-      var checkboxArray = document.getElementById('waypoints');
-      for (var i = 0; i < checkboxArray.length; i++) {
-        if (checkboxArray.options[i].selected) {
-          waypts.push({
-            location: checkboxArray[i].value,
-            stopover: true
-          });
-        }
+
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+      var route = response.routes[0];
+      var summaryPanel = document.getElementById('directions-panel');
+      summaryPanel.innerHTML = '';
+      // For each route, display summary information.
+      for (var i = 0; i < route.legs.length; i++) {
+        var routeSegment = i + 1;
+        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+            '</b><br>';
+        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
       }
-
-        directionsService.route({
-        origin: {lat:40.703104, lng:-74.016847},
-        destination: {lat: 40.850089, lng:-73.946785},
-        waypoints:waypts,
-        optimizeWaypoints:true,
-        travelMode:'BICYCLING'
-
-
-      }, function(response, status) {
-        if (status === 'OK') {
-          directionsDisplay.setDirections(response);
-          var route = response.routes[0];
-          var summaryPanel = document.getElementById('directions-panel');
-          summaryPanel.innerHTML = '';
-          // For each route, display summary information.
-          for (var i = 0; i < route.legs.length; i++) {
-            var routeSegment = i + 1;
-            summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-                '</b><br>';
-            summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-            summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-            summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-          }
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
-      });
+    } else {
+      window.alert('Directions request failed due to ' + status);
     }
+  });
+}
 
-  </script>
-  <script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAde5aRQ262GgOgdqkY8Jiu5KzYQYj2YKg&callback=initMap">
-  </script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAde5aRQ262GgOgdqkY8Jiu5KzYQYj2YKg&callback=initMap">
+</script>
