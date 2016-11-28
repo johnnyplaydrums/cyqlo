30a12a9be193cfb8ff1d75f8b9c4444cc2d9d7fa
Sammauel <S.Quintan4@gmail.com>
2016-11-28 13:52:47 -0500

    Updated the recent changes to test_views.py

--- main/tests/unittests/test_views.py
+++ main/tests/unittests/test_views.py
@@ -7,6 +7,18 @@ class TestPage(TestCase):
     def setUp(self):
         self.client = Client()
 
+"""
+The reverse() allows the unit testing to not compromise Don't Repeat Yourself
+or DRY when editting our code.  In the event of changes to the url.py this
+unit test allows us to grab the intended url and checks its name attribute.
+
+The response.status_code checks if the url is 200 was successful in grabbing
+the client request.
+
+The assertTemplateUsed() checks for asserts that the template with the given
+name was used in rendering the response.
+"""
+
     def test_index_page(self):
         url = reverse('index')
         response = self.client.get(url)
@@ -38,3 +50,9 @@ class TestPage(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'routes.html')
+
+    def test_west_side(self):
+        url = reverse('west_side_route')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'west_side_route.html')
