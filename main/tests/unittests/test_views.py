3ac26306b7270661fd9d6433e73960c8eb7b86a5
Sammauel <S.Quintan4@gmail.com>
2016-11-28 13:36:17 -0500

    Added test for about and routes page

--- main/tests/unittests/test_views.py
+++ main/tests/unittests/test_views.py
@@ -26,3 +26,15 @@ class TestPage(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup.html')
+
+    def test_about_page(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_route_page(self):
+        url = reverse('routes')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'routes.html')
