f647ab595bddf71e2ddff8b0aa55e32eac249279
Sammauel <S.Quintan4@gmail.com>
2016-11-30 16:15:43 -0500

    Added a test form for blank registration.

--- main/tests/unittests/test_forms.py
+++ main/tests/unittests/test_forms.py
@@ -1,19 +1,34 @@
 from django.test import TestCase
 from main.forms import RegistrationForm
-from main.models import User
 
 class FormsTestPage(TestCase):
 
     def test_Melissa_form(self):
-        mel = User(first_name ="Melissa", last_name="Banks", email="mbanks@email.com",
-                password="mbanks123")
-        mel_form = RegistrationForm({'first_name': 'Melissa', 'last_name': 'Banks', 'email': 'mbanks@email.com',
-                'password': 'mbanks123'}, instance = mel)
-        self.assertEqual(mel_form.is_valid(), True)
+        form = RegistrationForm({'first_name': 'Melissa', 'last_name': 'Banks', 'email': 'mbanks@email.com',
+                'password': 'mbanks123'})
+        self.assertTrue(form.is_valid())
+        register = form.save()
+        self.assertEqual(register.first_name, 'Melissa')
+        self.assertEqual(register.last_name, 'Banks')
+        self.assertEqual(register.email, 'mbanks@email.com')
+        self.assertEqual(register.password, 'mbanks123')
 
     def test_Tom_form(self):
-        tom = User(first_name ="Tom", last_name="Barnett", email="tbarnett@email.com",
-                password="tbarnett123")
-        tom_form = RegistrationForm({'first_name': '', 'last_name': 'Barnett', 'email': 'tbarnett@email.com',
-                'password': 'tbarnett123'}, instance = tom)
-        self.assertEqual(tom_form.is_valid(), False)
+        form = RegistrationForm({'first_name': 'Tom', 'last_name': 'Barnett', 'email': 'tbarnett@email.com',
+                'password': 'tbarnett123'})
+        self.assertTrue(form.is_valid())
+        register = form.save()
+        self.assertEqual(register.first_name, 'Tom')
+        self.assertEqual(register.last_name, 'Barnett')
+        self.assertEqual(register.email, 'tbarnett@email.com')
+        self.assertEqual(register.password, 'tbarnett123')
+
+    def test_blank_form(self):
+        form = RegistrationForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'first_name' : ['This field is required.'],
+            'last_name' : ['This field is required.'],
+            'email' : ['This field is required.'],
+            'password' : ['This field is required.']
+        })
