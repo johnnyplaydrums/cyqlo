e4cc278c80d8e0e41fb3bbad0d5caaf2d7ad5790
wujunhua <junhua.wu@outlook.com>
2016-11-30 17:38:14 -0500

    New route: shore_parkway

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^signup', views.signup, name='signup'),
     url(r'^about', views.about, name='about'),
     url(r'^routes', views.routes, name='routes'),
-    url(r'^west_side_route', views.west_side_route, name='west_side_route')
+    url(r'^west_side_route', views.west_side_route, name='west_side_route'),
+    url(r'^shore_parkway_route', views.shore_parkway_route, name='shore_parkway_route')
 ]
