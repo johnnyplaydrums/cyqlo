da96c2ff5b01a72c530aa0e6b3b816f367ff734c
wujunhua <junhua.wu@outlook.com>
2016-12-01 15:56:29 -0500

    pylint fix

--- main/urls.py
+++ main/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     url(r'^login', views.login, name='login'),
     url(r'^signup', views.signup, name='signup'),
     url(r'^about', views.about, name='about'),
-    url(r'^routes', views.routes, name='routes'),
+    url(r'^routes_page', views.routes_page, name='routes_page'),
     url(r'^west_side_route', views.west_side_route, name='west_side_route'),
-    url(r'^greenBronx_route', views.greenBronx_route, name='greenBronx_route'),
+    url(r'^green_bronx_route', views.green_bronx_route, name='green_bronx_route'),
     url(r'^shore_parkway_route', views.shore_parkway_route, name='shore_parkway_route'),
     url(r'^pizza_tour_route', views.pizza_tour_route, name='pizza_tour_route'),
     url(r'^columbuscircle_bearmtn_route', views.columbuscircle_bearmtn_route, name='columbuscircle_bearmtn_route')
-    
+
 ]
