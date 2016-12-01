17f0382d36d36d113c43773255ffd44185d6df61
DanObi <danobi06@gmail.com>
2016-12-01 15:20:18 -0500

    updating routes

--- main/urls.py
+++ main/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     url(r'^routes', views.routes, name='routes'),
     url(r'^west_side_route', views.west_side_route, name='west_side_route'),
     url(r'^greenBronx_route', views.greenBronx_route, name='greenBronx_route'),
-    url(r'^shore_parkway_route', views.shore_parkway_route, name='shore_parkway_route')
     url(r'^shore_parkway_route', views.shore_parkway_route, name='shore_parkway_route'),
     url(r'^pizza_tour_route', views.pizza_tour_route, name='pizza_tour_route'),
     url(r'^columbuscircle_bearmtn_route', views.columbuscircle_bearmtn_route, name='columbuscircle_bearmtn_route')
