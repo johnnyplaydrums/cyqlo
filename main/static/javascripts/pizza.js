6e79d0bb100cefb86de2d4c841ebb846e13ec3a6
mhendri <mhendrickson91@me.com>
2016-12-01 13:24:17 -0500

    location services for pizza route

--- main/static/javascripts/pizza.js
+++ main/static/javascripts/pizza.js
@@ -1,61 +1,77 @@
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'));
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.774022, lng: -73.970599},
+    zoom: 12
+  });
   directionsDisplay.setMap(map);
+  var infoWindow = new google.maps.InfoWindow({map: map});
 
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Current Location');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
   var button = document.getElementById("buttonid");
   var control = document.getElementById('floating-panel');
   control.style.display = 'block';
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(control);
+  //calculateAndDisplayRoute(directionsService, directionsDisplay);
 
-  button.addEventListener("click", function() {
-  directionsDisplay.setPanel(document.getElementById('right-panel'));
-  });
-
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
   document.getElementById('submit').addEventListener('click', function() {
   calculateAndDisplayRoute(directionsService, directionsDisplay);
   });
 }
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-  var waypts = [];
-  var checkboxArray = document.getElementById('waypoints');
-  for (var i = 0; i < checkboxArray.length; i++) {
-    if (checkboxArray.options[i].selected) {
-      waypts.push({
-        location: checkboxArray[i].value,
-        stopover: true
-      });
-    }
-  }
-
-    directionsService.route({
-    origin: {lat:40.703104, lng:-74.016847},
-    destination: {lat: 40.850089, lng:-73.946785},
-    waypoints:waypts,
-    optimizeWaypoints:true,
-    travelMode:'BICYCLING'
-
-
-  }, function(response, status) {
-    if (status === 'OK') {
-      directionsDisplay.setDirections(response);
-      var route = response.routes[0];
-      var summaryPanel = document.getElementById('directions-panel');
-      summaryPanel.innerHTML = '';
-      // For each route, display summary information.
-      for (var i = 0; i < route.legs.length; i++) {
-        var routeSegment = i + 1;
-        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-            '</b><br>';
-        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-      }
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
+
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
+
+    var waypts = [];
+    var checkboxArray = document.getElementById('waypoints');
+    for (var i = 0; i < checkboxArray.length; i++) {
+      if (checkboxArray.options[i].selected) {
+        waypts.push({
+          location: checkboxArray[i].value,
+          stopover: true
+        });
+      }
+    }
+        directionsService.route({
+          origin: {lat: position.coords.latitude, lng: position.coords.longitude},
+          destination: {lat: position.coords.latitude, lng: position.coords.longitude},
+          waypoints:waypts,
+          optimizeWaypoints:true,
+          travelMode: 'BICYCLING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      })
+    }
