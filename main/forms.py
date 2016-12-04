47a28f46141add2bfab181a8354311ec3f1fa2f9
wujunhua <junhua.wu@outlook.com>
2016-12-04 17:18:35 -0500

    signup username error handle

--- main/forms.py
+++ main/forms.py
@@ -1,12 +1,20 @@
 """ Forms for the main app """
 
 from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
-class RegistrationForm(forms.Form):
+class RegistrationForm(forms.ModelForm):
     """ Registration form for registering users """
     username = forms.CharField(label="Username", widget=forms.TextInput())
     email = forms.EmailField(label="Email", widget=forms.TextInput())
     password = forms.CharField(label="Password", widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
 
 class LoginForm(forms.Form):
     """ Login form form for users login """
