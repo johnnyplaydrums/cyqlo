919d18b233bbcf21ddb9972d3eb708b9eb111e49
Sammauel <S.Quintan4@gmail.com>
2016-12-06 13:29:41 -0500

    Updated for username
    first_name and last_name have been changed to use username instead.
    Reflecting those changes.

--- main/tests/unittests/test_forms.py
+++ main/tests/unittests/test_forms.py
@@ -4,22 +4,20 @@ from main.forms import RegistrationForm
 class FormsTestPage(TestCase):
 
     def test_Melissa_form(self):
-        form = RegistrationForm({'first_name': 'Melissa', 'last_name': 'Banks',
+        form = RegistrationForm({'username': 'mbanks',
             'email': 'mbanks@email.com', 'password': 'mbanks123'})
         self.assertTrue(form.is_valid())
         register = form.save()
-        self.assertEqual(register.first_name, 'Melissa')
-        self.assertEqual(register.last_name, 'Banks')
+        self.assertEqual(register.username, 'mbanks')
         self.assertEqual(register.email, 'mbanks@email.com')
         self.assertEqual(register.password, 'mbanks123')
 
     def test_Tom_form(self):
-        form = RegistrationForm({'first_name': 'Tom', 'last_name': 'Barnett',
+        form = RegistrationForm({'username': 'tbarnett',
             'email': 'tbarnett@email.com', 'password': 'tbarnett123'})
         self.assertTrue(form.is_valid())
         register = form.save()
-        self.assertEqual(register.first_name, 'Tom')
-        self.assertEqual(register.last_name, 'Barnett')
+        self.assertEqual(register.username, 'tbarnett')
         self.assertEqual(register.email, 'tbarnett@email.com')
         self.assertEqual(register.password, 'tbarnett123')
 
@@ -27,8 +25,7 @@ class FormsTestPage(TestCase):
         form = RegistrationForm({})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors, {
-            'first_name' : ['This field is required.'],
-            'last_name' : ['This field is required.'],
+            'username' : ['This field is required.'],
             'email' : ['This field is required.'],
             'password' : ['This field is required.']
         })
