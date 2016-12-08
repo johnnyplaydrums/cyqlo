10c3ec39c2f9353e918e71f84bf03fa5698a343a
John Spencer <johnspencer15@gmail.com>
2016-12-08 03:20:18 -0500

    un-require some routes fields, pad top of every page

--- main/models.py
+++ main/models.py
@@ -8,17 +8,17 @@ class Route(models.Model):
     """ Route model """
     route_name = models.CharField(max_length=50, null=False, blank=False)
     # duration of bike trip, in minutes
-    duration = models.IntegerField(null=True, blank=False)
+    duration = models.IntegerField(null=True, blank=True)
     # distance of bike trip
-    distance = models.FloatField(null=True, blank=False)
+    distance = models.FloatField(null=True, blank=True)
     # difficult = {'beg', 'med', 'adv'}
     difficulty = models.CharField(max_length=3, null=True, blank=True)
     # [lat,lng]
-    origin = ArrayField(models.FloatField(null=False, blank=False), size=2)
+    origin = ArrayField(models.FloatField(null=False), blank=True, size=2)
     # [lat,lng]
-    destination = ArrayField(models.FloatField(null=False, blank=False), size=2)
+    destination = ArrayField(models.FloatField(null=False), blank=True, size=2)
     # [location1, location2, location3, ...]
-    waypoints = ArrayField(models.CharField(max_length=256, null=False, blank=True))
+    waypoints = ArrayField(models.CharField(max_length=256, null=False), blank=True)
     # route image
     image = models.CharField(max_length=50, null=True, blank=False)
     # template name
